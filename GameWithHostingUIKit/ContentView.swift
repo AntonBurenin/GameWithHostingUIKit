06b23c01342aeb197dcad569dad69a7cba3dcc92
AntonBurenin <burkinband@mail.ru>
2020-08-02 19:03:47 +0300

    Initial Commit

---
+++ GameWithHostingUIKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameWithHostingUIKit
+//
+//  Created by Anton on 02.08.2020.
+//  Copyright © 2020 Anton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
